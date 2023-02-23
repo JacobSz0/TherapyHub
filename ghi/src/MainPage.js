3a7a5f7de58c1ddb52c1a57dabe2cc03eb80be12
Kamron <kamronpoosti@yahoo.com>
2023-02-22 17:19:47 -0800

    prepped frontend for group

---
+++ ghi/src/MainPage.js
@@ -0,0 +1,14 @@
+function MainPage() {
+  return (
+    <div className="px-4 py-5 my-5 text-center">
+      <h1 className="display-5 fw-bold">TherapyHub</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">
+          Find your Therapist Today!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;

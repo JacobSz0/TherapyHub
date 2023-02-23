e730380bc6be81c36f17870947fbe319338fe19c
Kamron <kamronpoosti@yahoo.com>
2023-02-23 14:03:01 -0800

    adjusted color of background

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,13 +2,16 @@ import { useEffect, useState } from 'react';
 import Construct from './Construct.js'
 import ErrorNotification from './ErrorNotification';
 import MainPage from './MainPage.js';
+import logo from "./imgs/logo.png";
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
+
       <BrowserRouter>
+
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import logo from "./imgs/logo.png";
+//import $ from 'jquery';
 
 
 function MainPage() {
@@ -9,14 +11,26 @@ function MainPage() {
   // }, []);
 
   return (
+
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">TherapyHub</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="lead mb-4">
-          Find your Therapist Today!
-        </p>
-      </div>
+
+      <div style={{
+      backgroundImage: `url(${logo})`,
+      backgroundSize: 'cover',
+      height: '600px',
+      width: '100%'
+    }}>
+
     </div>
+    </div>
+    //   <div className="px-4 py-5 my-5 text-center">
+    //  <h1 className="display-5 fw-bold">TherapyHub</h1>
+    //   <div className="col-lg-6 mx-auto">
+    //     <p className="lead mb-4">
+    //       Find your Therapist Today!
+    //     </p>
+    //   </div>
+    //</div>
     //    <select id="example-multiple-selected" multiple ref={selectRef}>
     //   <option value="1">Option 1</option>
     //   <option value="2" selected>Option 2</option>
